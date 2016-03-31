18c33bc50e5bde24bef82b37d6c585465d140ecd
Chris Walker <cwalkatron@users.noreply.github.com>
2016-03-31 10:00:01 -0700

    more stuff

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift Ninjas</h1>"
+    out = ""
+    for i in range(20):
+        out += " {0:02X}".format(i)
+    return "<h1> hello OpenShift Ninjas</h1>" + "<code>{0}</code>".format(out)
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
